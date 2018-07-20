4cca0806e4fa2590c6b4bf637df2dafa4aee3d4e
ludanxer <ludanxer@gmail.com>
2018-07-20 15:57:40 +0800

    fix update

--- MvcMovie/MvcMovie/DAL/MovieDBContext.cs
+++ MvcMovie/MvcMovie/DAL/MovieDBContext.cs
@@ -23,18 +23,4 @@ namespace MvcMovie.DAL
         public DbSet<Movie> Movies { get; set; }
     }
 
-    public class MovieInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<MovieDBContext>
-    {
-        protected override void Seed(MovieDBContext context)
-        {
-            var movies = new List<Movie>
-            {
-            new Movie{ID=1,Title="Alexander",ReleaseDate=DateTime.Parse("2005-09-01"),Genre="f",Price=210},
-            new Movie{ID=2,Title="Tom",ReleaseDate=DateTime.Parse("2018-09-01"),Genre="c",Price=110}
-            };
-
-            movies.ForEach(s => context.Movies.Add(s));
-            context.SaveChanges();
-        }
-    }
 }
