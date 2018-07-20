68d7e2736811fd9d4d5ad70725399ce9ba0ec6ab
pdzsbl <2823694360@qq.com>
2018-07-20 16:41:27 +0800

    OrderdetailIDS->Orderdetailid

--- MvcMovie/MvcMovie/DAL/MovieDBContext.cs
+++ MvcMovie/MvcMovie/DAL/MovieDBContext.cs
@@ -28,7 +28,7 @@ namespace MvcMovie.DAL
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderDetail> OrderDetailIDs { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<ReceivingInfo> ReceivingInfos { get; set; }
         public DbSet<Saler> Salers { get; set; }
         public DbSet<Shop> Shops { get; set; }
