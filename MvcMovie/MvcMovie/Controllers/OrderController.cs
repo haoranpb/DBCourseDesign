e26e40e395add24b630bc0d75137e024bec0d3e3
ludanxer <ludanxer@gmail.com>
2018-07-20 16:25:41 +0800

    commit controllers

---
+++ MvcMovie/MvcMovie/Controllers/OrderController.cs
@@ -0,0 +1,69 @@
+﻿using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class OrderController : Controller
+    {
+        OrderDBContext db = new OrderDBContext();
+        ItemDBContext idb = new ItemDBContext();
+        OrderItemDBContext oidb = new OrderItemDBContext();
+        CartDBContext cdb = new CartDBContext();
+      
+        public ActionResult Index(string id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "OrderID,ShopID ,CustomerID,OrderPrice , OrderCount ,OrderTime ,OrderState")] Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+        
+            var iteminfo = (from c in oidb.OrderItems where c.OrderID == order.OrderID select c);
+            foreach (var iteml in iteminfo)
+            {
+                Item itemp = idb.Items.Find(iteml.ItemID);
+                itemp.ItemRemain = itemp.ItemRemain - 1;
+                idb.Entry(itemp).State = EntityState.Modified;
+                idb.SaveChanges();
+
+
+                var cartinfo = (from j in cdb.Carts where j.CartID == iteml.ItemID && j.CustomerID == order.CustomerID select j);
+                foreach (var cartitem in cartinfo)
+                {
+                    cdb.Carts.Remove(cartitem);
+                    db.SaveChanges();
+                }//删除购物车内的商品
+            }
+         
+            Item item = idb.Items.Find();
+            return Content("success") ;
+        }
+
+        public ActionResult Query(int Orderid)
+        {
+            Order od = db.Orders.Find(Orderid);
+            var iteminfo = (from c in oidb.OrderItems where c.OrderID == Orderid select c);
+            int number = 0;
+            string idstring = "";
+            string sumstring = "";
+            foreach (var iteml in iteminfo)
+            {
+                number++;
+                idstring = String.Join (idstring," ",iteml.ItemID);
+                sumstring = String.Join(sumstring, " ", iteml.OrderDetailCount);
+            }
+            var P = new { number = number.ToString(), idstring = idstring, sumstring = sumstring };
+                return Json(P);
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Controllers/PersonalinfoController.cs
@@ -0,0 +1,76 @@
+﻿using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//全部使用localdb的连接方式，而非oracle
+namespace MvcMovie.Controllers
+{
+    public class PersonalinfoController : Controller
+    {
+        private CustomerDBContext cdb = new CustomerDBContext();
+        private SalerDBContext sdb = new SalerDBContext();
+        private AdminDBContext adb = new AdminDBContext();
+        // GET: Personalinfo/Index?ID=XXX&role=xxxxx
+        public ActionResult Index(string ID)
+        {
+            Customer cus = cdb.Customers.Find(ID);
+            Admin adm = adb.Admins.Find(ID);
+            Saler sel = sdb.Salers.Find(ID);
+            int role = 0;
+            if (adm != null) role = 1;
+            if (sel != null) role = 2;
+            //根据id在数据库内查找
+            //尝试使用viewbag对页面传参
+            switch (role)
+            {
+                case 0://顾客
+                    ViewBag.age = cus.CustomerAge;
+                    ViewBag.key = cus.CustomerPassword;
+                    ViewBag.id = ID;
+                    break;
+                case 1://管理
+                    ViewBag.key = adm.AdminPassword;
+                    ViewBag.id = ID;
+                    break;
+                case 2://卖家
+                    ViewBag.salerinfo = sel.SalerInfo;
+                    ViewBag.key = sel.SalerPassword;
+                    ViewBag.id = ID;
+                    ViewBag.phone = sel.SalerPhone;
+                    break;
+            }
+            return View();
+        }
+        // Post:/Personalinfo/EditCus
+        [HttpPost]
+        public ActionResult EditCus([Bind(Include = "ID,CustomerAge,CustomerPassword")] Customer customer)
+        {
+            cdb.Entry(customer).State = EntityState.Modified;
+            cdb.SaveChanges();
+            return Content("success");
+        }
+        // Post:/Personalinfo/EditAdm
+        [HttpPost]
+        public ActionResult EditAdm([Bind(Include = "AdminID,AdminPassword")] Admin admin)
+        {
+            adb.Entry(admin).State = EntityState.Modified;
+            adb.SaveChanges();
+            return Content("success");
+        }
+        // Post:/Personalinfo/EditSel
+        [HttpPost]
+        public ActionResult EditSel([Bind(Include = "SalerID,SalerPassword,SalerPhone,SalerInfo")] Saler seller)
+        {
+            sdb.Entry(seller).State = EntityState.Modified;
+            sdb.SaveChanges();
+            return Content("success");
+        }
+
+
+    }
+
+
+}
