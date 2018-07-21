0517b2f6763caaf18df2a2c160e1f5eb66651f54
NaiveBNB <1215048107@qq.com>
2018-07-21 13:07:43 +0800

    Add files via upload

--- MvcMovie/MvcMovie/Controllers/HomePageController.cs
+++ MvcMovie/MvcMovie/Controllers/HomePageController.cs
@@ -7,66 +7,87 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MvcMovie.Models;
+using Oracle.ManagedDataAccess.Client;
+using MvcMovie.DAL;
 
 namespace MvcMovie.Controllers
 {
-    public class HomePageController : Controller
+    public class HomePagesController : Controller
     {
-        private ShopDBContext sdb = new ShopDBContext();
-        private GoodsDBContext gdb = new GoodsDBContext();
+        private MovieDBContext db = new MovieDBContext();
 
         // GET: Recommend
+        // 
         public ActionResult Index()
         {
             ViewBag.Message = "Vibranium Home Page.";
-            Response.Redirect("http://localhost:52510/HomePage");
+
             return View();
         }
 
         // GET: Recommend/Shop
-        public ActionResult Shop()
+        public ActionResult ShopInfo(int? id)
         {
-            ViewBag.Message = "Shop Page.";
-
-            return View();
-        }
-
-        // GET: Recommend/Goods
-        public ActionResult Goods()
-        {
-            ViewBag.Message = "Goods Page.";
-
-            return View();
-        }
-
-        // GET: Recommend/Discount
-        public ActionResult Discount()
-        {
-            ViewBag.Message = "Discount Page.";
-
-            return View();
-        }
-
-        // GET: Recommend/SearchShop
-        public ActionResult SearchShop(string id)
-        {
-            Shop shop = sdb.Shop.Find(id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shop shop = db.Shops.Find(id);
             if (shop == null)
             {
                 return HttpNotFound();
             }
-            return Redirect(@"http://localhost:52510/Shop/id=？");
+            return View();
+        }
+
+        // GET: Recommend/Goods
+        public ActionResult GoodsInfo(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
+
+        // GET: Recommend/SearchShop
+        public ActionResult SearchShop(string name)
+        {
+            Shop shop = db.Shops.Find(name);
+            if (shop == null)
+            {
+                return HttpNotFound();
+            }
+            var ShopInfo = new { Name = Shop.ShopName, ID = Shop.ShopID,
+                Saler = Shop.SalerID, Level = Shop.SalerCredit };
+            return Json(ShopInfo);
         }
 
         // GET: Recommend/SearchGoods
-        public ActionResult SearchGoods(string id)
+        public ActionResult SearchGoods(string name)
         {
-            Goods goods = gdb.Goods.Find(id);
+            Item item = db.Items.Find(name);
             if (goods == null)
             {
                 return HttpNotFound();
             }
-            return Redirect(@"http://localhost:52510/goods/id=？");
+            var GoodsInfo = new
+            {
+                GID = Item.ItemID,
+                SID = Item.ShopID,
+                Price = Item.ItemPrice,
+                Name = Item.ItemPrice,
+                Info = Item.ItemInfo,
+                Pic = Item.ItemPic,
+                Sale = Item.Sales,
+                Quanity = Item.Remain
+            };
+            return Json(GoodsInfo);
         }
     }
 }
