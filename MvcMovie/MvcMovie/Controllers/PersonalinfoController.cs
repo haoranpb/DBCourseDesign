ad1c46c172c0613feccd9817fc421e190a36e8c4
ludanxer <ludanxer@gmail.com>
2018-07-22 20:28:18 +0800

    Revert "Personalinfo修改完成"
    This reverts commit c287e6527fba723b55de14a62b95e479b2129cff.

--- MvcMovie/MvcMovie/Controllers/PersonalinfoController.cs
+++ MvcMovie/MvcMovie/Controllers/PersonalinfoController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcMovie.DAL;
-//这个页面针对的是买家
+//全部使用localdb的连接方式，而非oracle
 namespace MvcMovie.Controllers
 {
     public class PersonalinfoController : Controller
@@ -16,77 +16,57 @@ namespace MvcMovie.Controllers
         public ActionResult Index(string ID)
         {
             Customer cus = db.Customers.Find(ID);
-            //Admin adm = db.Admins.Find(ID);
-            //Saler sel = db.Salers.Find(ID);
-            //int role = 0;
-            //if (adm != null) role = 1;
-            //if (sel != null) role = 2;
+            Admin adm = db.Admins.Find(ID);
+            Saler sel = db.Salers.Find(ID);
+            int role = 0;
+            if (adm != null) role = 1;
+            if (sel != null) role = 2;
             //根据id在数据库内查找
             //尝试使用viewbag对页面传参
-            //switch (role)
-            //{
-            //    case 0://顾客
+            switch (role)
+            {
+                case 0://顾客
                     ViewBag.age = cus.CustomerAge;
                     ViewBag.key = cus.CustomerPassword;
                     ViewBag.id = ID;
-            //        break;
-            //    case 1://管理
-            //        ViewBag.key = adm.AdminPassword;
-            //        ViewBag.id = ID;
-            //        break;
-            //    case 2://卖家
-            //        ViewBag.salerinfo = sel.SalerInfo;
-            //        ViewBag.key = sel.SalerPassword;
-            //        ViewBag.id = ID;
-            //        ViewBag.phone = sel.SalerPhone;
-            //        break;
-            //}
+                    break;
+                case 1://管理
+                    ViewBag.key = adm.AdminPassword;
+                    ViewBag.id = ID;
+                    break;
+                case 2://卖家
+                    ViewBag.salerinfo = sel.SalerInfo;
+                    ViewBag.key = sel.SalerPassword;
+                    ViewBag.id = ID;
+                    ViewBag.phone = sel.SalerPhone;
+                    break;
+            }
             return View();
         }
         // Post:/Personalinfo/EditCus
-        //[HttpPost]
-        //public ActionResult EditCus([Bind(Include = "ID,CustomerAge,CustomerPassword")] Customer customer)
-        //{
-        //    db.Entry(customer).State = EntityState.Modified;
-        //    db.SaveChanges();
-        //    return Content("success");
-        //}
-
-        //get:/Personalinfo/EditCus?ID=222&CustomerAge=38&CustomerGender=girl&CustomerPassword=qqq&CustomerPhone=111
-        public ActionResult EditCus()//修改信息
+        [HttpPost]
+        public ActionResult EditCus([Bind(Include = "ID,CustomerAge,CustomerPassword")] Customer customer)
         {
-            string ID = Request.QueryString["ID"];
-            string CustomerAge = Request.QueryString["CustomerAge"];
-            string CustomerPassword = Request.QueryString["CustomerPassword"];
-            string CustomerGender = Request.QueryString["CustomerGender"];
-            string CustomerPhone = Request.QueryString["CustomerPhone"];
-            Customer customer = db.Customers.Find(ID);
-            customer.CustomerAge = int.Parse(CustomerAge);
-            customer.CustomerPassword = CustomerPassword;
-            customer.CustomerGender = CustomerGender;
-            customer.CustomerPhone = CustomerPhone;
             db.Entry(customer).State = EntityState.Modified;
             db.SaveChanges();
-            return View(); //页面不跳转，应在前端提示成功
+            return Content("success");
         }
-
-
-        //// Post:/Personalinfo/EditAdm
-        //[HttpPost]
-        //public ActionResult EditAdm([Bind(Include = "AdminID,AdminPassword")] Admin admin)
-        //{
-        //    db.Entry(admin).State = EntityState.Modified;
-        //    db.SaveChanges();
-        //    return Content("success");
-        //}
-        //// Post:/Personalinfo/EditSel
-        //[HttpPost]
-        //public ActionResult EditSel([Bind(Include = "SalerID,SalerPassword,SalerPhone,SalerInfo")] Saler seller)
-        //{
-        //    db.Entry(seller).State = EntityState.Modified;
-        //    db.SaveChanges();
-        //    return Content("success");
-        //}
+        // Post:/Personalinfo/EditAdm
+        [HttpPost]
+        public ActionResult EditAdm([Bind(Include = "AdminID,AdminPassword")] Admin admin)
+        {
+            db.Entry(admin).State = EntityState.Modified;
+            db.SaveChanges();
+            return Content("success");
+        }
+        // Post:/Personalinfo/EditSel
+        [HttpPost]
+        public ActionResult EditSel([Bind(Include = "SalerID,SalerPassword,SalerPhone,SalerInfo")] Saler seller)
+        {
+            db.Entry(seller).State = EntityState.Modified;
+            db.SaveChanges();
+            return Content("success");
+        }
 
 
     }
