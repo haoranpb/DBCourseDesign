fbfe0b579cd3acb55a2b06400e411adc5fa6c844
ludanxer <ludanxer@gmail.com>
2018-07-23 00:09:05 +0800

    upload AdminInfoController and doc

---
+++ Docs/AdminInfoCotroller API.md
@@ -0,0 +1,15 @@
+// GET: AdminInfo/Index
+
+return Content("success") // finish redirect url in the front-end. Qustion: need to pass admin id through index func? 
+
+
+
+// GET: AdminInfo/DeleteSaler/id 
+
+return Content("success"); // redirect to index via front-end 
+
+
+
+// GET: AdminInfo/DeleteCustomer/id
+
+return Content("success"); // redirect to index via front-end 
---
+++ MvcMovie/MvcMovie/Controllers/AdminInfoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+using MvcMovie.DAL;
+
+namespace MvcMovie.Controllers
+{
+    public class AdminInfoController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        // GET: AdminInfo/Index
+        public ActionResult Index()
+        {
+            ViewBag.SalerList = db.Salers.ToList();
+            ViewBag.CustomerList = db.Customers.ToList();
+
+            return Content("success"); // finish redirect url in the front-end. Qustion: need to pass admin id through index func?
+        }
+
+        // GET: AdminInfo/DeleteSaler/id
+        public ActionResult DeleteSaler(string id)
+        {
+            Saler saler = db.Salers.Find(id);
+            db.Salers.Remove(saler);
+            return Content("success"); // redirect to index via front-end
+        }
+
+        // GET: AdminInfo/DeleteCustomer/id
+        public ActionResult DeleteCustomer(string id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            return Content("success"); // redirect to index via front-end
+        }
+    }
+}
