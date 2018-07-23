28c4d42fad049601dd88e6467bd6d241eef253c8
Lin-Lei <873003680@qq.com>
2018-07-23 12:09:06 +0800

    Homepage添加搜索功能
    注释无用代码，添加搜索功能

--- MvcMovie/MvcMovie/Controllers/HomePageController.cs
+++ MvcMovie/MvcMovie/Controllers/HomePageController.cs
@@ -16,81 +16,75 @@ namespace MvcMovie.Controllers
     {
         private MovieDBContext db = new MovieDBContext();
 
-        // GET: Recommand
-        // Home页面
+        // GET: /Homepage/Index
         public ActionResult Index()
         {
-            ViewBag.Message = "Vibranium Home Page.";
-            ViewBag.id = Request.QueryString["id"];
             return PartialView();
         }
 
-        // GET: Recommend/Shop
-        // 点击进入店铺页
-        public ActionResult ShopInfo(string id)
-        {
-            string user_id = ViewBag.ID;
-            Shop shop = db.Shops.Find(id);
-            if (shop == null)
-            {
-                return HttpNotFound();
-            }
-            string url = string.Join("~/Shop/Index", id);
-            return RedirectToRoute("url & ID = user_id");
-        }
+        //// GET: Recommend/Shop
+        //// 点击进入店铺页
+        //public ActionResult ShopInfo(string id)
+        //{
+        //    string user_id = ViewBag.ID;
+        //    Shop shop = db.Shops.Find(id);
+        //    if (shop == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    string url = string.Join("~/Shop/Index", id);
+        //    return RedirectToRoute("url & ID = user_id");
+        //}
 
-        // GET: Recommend/Goods
-        // 点击进入商品页
-        public ActionResult ItemInfo(string id)
-        {
-            string user_id = ViewBag.ID;
-            Item item = db.Items.Find(id);
-            if (item == null)
-            {
-                return HttpNotFound();
-            }
-            string url = string.Join("~/Item/Index", id);
-            return RedirectToRoute("url & ID = user_id");
-        }
+        //// GET: Recommend/Goods
+        //// 点击进入商品页
+        //public ActionResult ItemInfo(string id)
+        //{
+        //    string user_id = ViewBag.ID;
+        //    Item item = db.Items.Find(id);
+        //    if (item == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    string url = string.Join("~/Item/Index", id);
+        //    return RedirectToRoute("url & ID = user_id");
+        //}
 
-        // GET: Recommend/SearchShop
-        // 搜索店铺
-        public ActionResult SearchShop(string name)
-        {
-            Shop shop = db.Shops.Find(name);
-            if (shop == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.Name = shop.ShopName;
-            ViewBag.ShopID = shop.ShopID;
-            ViewBag.Saler = shop.SalerID;
-            ViewBag.Level = shop.SalerCredit;
+        //// GET: Recommend/SearchShop
+        //// 搜索店铺
+        //public ActionResult SearchShop(string name)
+        //{
+        //    Shop shop = db.Shops.Find(name);
+        //    if (shop == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.Name = shop.ShopName;
+        //    ViewBag.ShopID = shop.ShopID;
+        //    ViewBag.Saler = shop.SalerID;
+        //    ViewBag.Level = shop.SalerCredit;
 
-            string url = string.Join("~/Shop/Index", name);
-            return RedirectToRoute("url & ID = user_id");
-        }
+        //    string url = string.Join("~/Shop/Index", name);
+        //    return RedirectToRoute("url & ID = user_id");
+        //}
 
-        // GET: Recommend/SearchGoods
+        // GET: /HomePage/Search?keyword=LILIFAN
         // 搜索商品
-        public ActionResult SearchGoods(string name)
+        public ActionResult Search(string keyword)
         {
-            Item item = db.Items.Find(name);
-            if (item == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.GID = item.ItemID;
-            ViewBag.SID = item.ShopID;
-            ViewBag.Price = item.ItemPrice;
-            ViewBag.Name = item.ItemPrice;
-            ViewBag.Info = item.ItemInfo;
-            ViewBag.Pic = item.ItemPic;
-            ViewBag.Sale = item.ItemSales;
-            ViewBag.Quanity = item.ItemRemain;
+            var items = from item in db.Items
+                        where item.ItemName.Contains(keyword)
+                        select item;
 
-            string url = string.Join("~/Item/Index", name);
-            return RedirectToRoute("url & ID = user_id");
+            //用于测试
+            string ItemIdString = "";
+            foreach (var item in items)
+            {
+                ItemIdString = String.Join(ItemIdString, "   ", item.ItemID);
+            }
+            return Content(ItemIdString);
+
+            //return View(items.ToList());
         }
     }
 }
