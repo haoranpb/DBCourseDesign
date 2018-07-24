6406b32a5bcf250c50e32946d67c2a334c75f427
Renovamen <renovamenzxh@gmail.com>
2018-07-24 09:38:03 +0800

    AdminInfoController功能完善

--- MvcMovie/MvcMovie/Controllers/AdminInfoController.cs
+++ MvcMovie/MvcMovie/Controllers/AdminInfoController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
 using System.Web.Mvc;
 using MvcMovie.Models;
 using MvcMovie.DAL;
+using System.Data.Entity;
 
 namespace MvcMovie.Controllers
 {
@@ -12,13 +14,33 @@ namespace MvcMovie.Controllers
     {
         private MovieDBContext db = new MovieDBContext();
 
-        // GET: AdminInfo/Index?id = xxx
-        public ActionResult Index()
+        // GET: AdminInfo/Index?adminID=xxx
+        public ActionResult Index(string adminID)
         {
             ViewBag.SalerList = db.Salers.ToList();
             ViewBag.CustomerList = db.Customers.ToList();
+            ViewBag.ItemList = db.Items.ToList();
+            ViewBag.OrderList = db.Orders.ToList();
+            ViewBag.AdminID = adminID;
             return Content("success"); // finish redirect url in the front-end. Qustion: need to pass admin id through index func?
         }
+
+        // GET: AdminInfo/AdminInfo?adminID=xxx
+        //管理员信息
+        public ActionResult AdminInfo(string adminID)
+        {
+            Admin admin = db.Admins.Find(adminID);
+            if (admin == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var AdminInfo = new
+            {
+                ID = admin.AdminID,
+                Info = admin.AdminInfo,
+            };
+            return Json(AdminInfo);
+        }
+
+
 
         // GET: AdminInfo/DeleteSaler/id
         public ActionResult DeleteSaler(string id)
@@ -38,33 +60,38 @@ namespace MvcMovie.Controllers
             return Content("success"); // redirect to index via front-end
         }
 
-        // GET:  AdminInfo/Search/ID
-        // 根据ID搜索
-        public ActionResult Search(string ID)
+        // GET: AdminInfo/DeleteItem/id
+        public ActionResult DeleteItem(string id)
         {
-            //先在买家中查找
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return Content("success"); // redirect to index via front-end
+        }
+
+        // GET: AdminInfo/DeleteOrder/id
+        public ActionResult DeleteOrder(string id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return Content("success"); // redirect to index via front-end
+        }
+
+        // GET:  AdminInfo/SearchCustomer/ID
+        // 根据ID搜索
+        public ActionResult SearchCustomer(string ID)
+        {
             Customer customer = db.Customers.Find(ID);
-            if (customer == null)//买家没找到 则在卖家中查找
-            {
-                Saler saler = db.Salers.Find(ID);
-                if (saler == null) return Content("not found");//卖家中也没找到 则返回未找到信息
-                else//返回找到的卖家信息
-                {
-                    var SalerInfo = new
-                    {
-                        Type = "saler",
-                        ID = saler.SalerID,
-                        Info = saler.SalerInfo,
-                        Phone = saler.SalerPhone,
-                    };
-                    return Json(SalerInfo);
-                }
-            }
-            else//返回找到的买家信息
+            Saler saler = db.Salers.Find(ID);
+            Item item = db.Items.Find(ID);
+            Order order = db.Orders.Find(ID);
+
+            if (customer == null) return Content("not found"); //没找到
+            else
             {
                 var CustomerInfo = new
                 {
-                    Type = "customer",
                     ID = customer.ID,
                     Age = customer.CustomerAge,
                     Gender = customer.CustomerGender,
@@ -75,6 +102,82 @@ namespace MvcMovie.Controllers
             }
             // url重定向在前端实现 
         }
+
+
+
+        // GET:  AdminInfo/SearchSaler/ID
+        // 根据ID搜索
+        public ActionResult SearchSaler(string ID) //搜索卖家
+        {
+            Saler saler = db.Salers.Find(ID);
+
+            if (saler == null) return Content("not found"); //没找到
+            else
+            {
+                var ShopIDs = from shops in db.Shops where shops.SalerID == ID select shops;
+                var SalerInfo = new
+                {
+                    ID = saler.SalerID,
+                    Info = saler.SalerInfo,
+                    Phone = saler.SalerPhone,
+                    ShopList = ShopIDs.ToList(),
+                };
+                return Json(SalerInfo);
+            }
+            // url重定向在前端实现 
+        }
+
+
+        // GET:  AdminInfo/SearchItem/ID
+        // 根据ID搜索
+        public ActionResult SearchItem(string ID) //搜索商品
+        {
+            Item item = db.Items.Find(ID);
+
+            if (item == null) return Content("not found"); //没找到
+            else
+            {
+                var ItemInfo = new
+                {
+                    ItemID = item.ItemID,
+                    ShopID = item.ShopID,
+                    Price = item.ItemPrice,
+                    Name = item.ItemName,
+                    Info = item.ItemInfo,
+                    Pic = item.ItemPic,
+                    Sales = item.ItemSales,
+                    Remain = item.ItemRemain,
+
+                };
+                return Json(ItemInfo);
+            }
+            // url重定向在前端实现 
+        }
+
+        // GET:  AdminInfo/SearchOrder/ID
+        // 根据ID搜索
+
+        public ActionResult SearchOrder(string ID) //搜索订单
+        {
+            Item item = db.Items.Find(ID);
+
+            var OrderIDs = from orders in db.Orders where orders.OrderID == ID select orders; //可能会有多个订单号相同     
+
+            var orderlist = "";
+            foreach (var order in OrderIDs)
+            {
+                orderlist = orderlist + "#" + order.OrderID + " " + order.ShopID + " " + order.CustomerID + " "
+                            + order.OrderState + " " + order.OrderPrice + " " + order.OrderCount;
+            }
+
+            if (orderlist == null) return Content("not found");
+            else return Content(orderlist);
+
+            // url重定向在前端实现 
+        }
+
+
+
 
     }
 }
